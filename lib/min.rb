0106d788b9f0c818550ba14b4d91d59bc1eda0b4
thisistheway369 <thisistheway369@icloud.com>
2020-02-02 05:59:35 -0800

    Done.

--- lib/min.rb
+++ lib/min.rb
@@ -1,13 +1,30 @@
 require 'pry'
 
 #This is the groceries hash we'll be passing in to the get_the_min method
-# groceries = {
-#  dairy: ["milk", "yogurt", "cheese"],
-#  vegetable: ["carrots", "broccoli", "cucumbers"],
-#  meat: ["chicken", "steak", "salmon"],
-#  grains: ["rice", "pasta"]
-# }
+groceries = {
+  dairy: ["milk", "yogurt", "cheese"],
+  vegetable: ["carrots", "broccoli", "cucumbers"],
+  meat: ["chicken", "steak", "salmon"],
+  grains: ["rice", "pasta"]
+ }
+#This is the groceries hash we'll be passing in to the get_the_values method
+
 
 def get_the_min(groceries)
-  #code your solution here!
+  groceries.values.flatten.min
+end
+
+#This is the groceries hash we'll be passing in to the get_the_values method
+groceries = {
+  dairy: ["milk", "yogurt", "cheese"],
+  vegetable: ["carrots", "broccoli", "cucumbers"],
+  meat: ["chicken", "steak", "salmon"],
+  grains: ["rice", "pasta"]
+}
+
+
+
+
+def get_the_min(groceries)
+  groceries.values.flatten.min
 end
